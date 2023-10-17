87e6d8ba1fc2e25e292091a56e1587d9c4bd2cfc
Ericdid <ericdidom@gmail.com>
2023-10-17 02:39:34 +0200

    add.view_working

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,6 +2,17 @@
 
 @section('main-content')
     <section class="container mt-5">
-        <h1>{{ $title }}</h1>
+        <div class="row">
+            @forelse ($movies as $movie)
+                <div class="col-3 mx-2 my-3 border">
+                    <h2> Titolo:{{ $movie->title }} </h2>
+                    <h4> Titolo Originale: {{ $movie->original_title }}</h4>
+                    <p>durata: {{ $movie->date }} </p>
+                    <p>voto: {{ $movie->vote }} </p>
+                </div>
+            @empty
+                <h2>Non ci sono Films</h2>
+            @endforelse
+        </div>
     </section>
 @endsection
