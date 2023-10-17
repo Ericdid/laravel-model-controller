dba21a72bdc4b058ae91c9c6fd429bf2e1ac93e4
Ericdid <ericdidom@gmail.com>
2023-10-17 03:03:06 +0200

    bonus.horrible_scss

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('main-content')
     <section class="container mt-5">
-        <div class="row">
+        <div class="row justify-content-around">
             @forelse ($movies as $movie)
-                <div class="col-3 mx-2 my-3 border">
-                    <h2> Titolo:{{ $movie->title }} </h2>
+                <div class="col-2 mx-2 my-3">
+                    <h2> {{ $movie->title }} </h2>
                     <h4> Titolo Originale: {{ $movie->original_title }}</h4>
                     <p>durata: {{ $movie->date }} </p>
                     <p>voto: {{ $movie->vote }} </p>
